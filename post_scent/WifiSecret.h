c8fede169b51e44e34b0c20ed35b251cdabcf335
Taku-Sashima <131934396+Taku-Sashima@users.noreply.github.com>
2024-02-15 13:59:52 +0900

    名称の変更

---
+++ post_scent/WifiSecret.h
@@ -0,0 +1,7 @@
+//Wifiのidとパスワード
+const char *ssid_secret = "O-202b";
+const char *pass_secret = "sagamihara";
+
+//サーバーを立てるPCのIPアドレスとポート
+const char *pc_addr_secret = "192.168.0.139";
+const char *pc_port_secret = "8000";
